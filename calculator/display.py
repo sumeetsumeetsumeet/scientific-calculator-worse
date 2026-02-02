7ee6a980d1f76d0035583f3b549866929eaf6394
ramneetkang-07 <ramneetkang2000@gmail.com>
2026-02-02 21:18:13 +0530

    Changing variable names for better documentation

--- calculator/display.py
+++ calculator/display.py
@@ -1,11 +1,11 @@
 from tkinter import Entry, StringVar
 
-def create_display(window):
-    input_equation_var = StringVar(master=window)
+def create_display(root_window):
+    input_equation_var = StringVar(master=root_window)
     input_equation_var.set("")
 
     display = Entry(
-        window,
+        root_window,
         textvariable=input_equation_var,
         font=("Consolas", 20),
         bg="white",
